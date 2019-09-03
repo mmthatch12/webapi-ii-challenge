db19231caa6582d697084e09a0ed3bb6c3364200
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-03 13:50:46 -0600

    working on first post

--- server.js
+++ server.js
@@ -2,6 +2,31 @@ const express = require('express')
 
 const server = express()
 
+const db = require('./data/db')
+
+server.use(express.json())
+
+server.post('/api/posts', (req, res) => {
+    const postInfro = req.body
+    
+    if(postInfro.title && postInfro.contents) {
+        db.insert(postInfro)
+        .then(dat => {
+            res.status(201).json(dat)
+        })
+        .catch(error => {
+            res.send(error)
+        })
+    } else {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+
+    db.insert()
+        .catch(error => {
+            res.status(500).json({ error: "There was an error while saving the post to the database" })
+        })
+})
+
 
 
 
